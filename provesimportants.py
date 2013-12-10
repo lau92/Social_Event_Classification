fa01299b241bafe8c9459e8d593486efb5fad6ae
sergi413 <sergi138@hotmail.com>
2013-12-10 10:58:10 +0100

    Delete provesimportants.py

--- provesimportants.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open('C:\Users\Iker\Desktop\Q5-7\GDSA\Classificador\sed2013_task2_dataset_train.csv', 'rb') as f:
-    reader = csv.reader(f)
-        for row in reader:
-            print row
